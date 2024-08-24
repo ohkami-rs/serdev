99be73162a1fe2112478171ee42c35ac99845e39
kana-rus <kanarus786@gmail.com>
2024-08-25 04:46:33 +0900

    I found validation in Serialize is not required!!!

--- serdev_derive/src/internal.rs
+++ serdev_derive/src/internal.rs
@@ -1,13 +1,12 @@
 mod target;
 mod validate;
-mod bound;
 
 use self::target::Target;
 use self::validate::Validate;
 
 use proc_macro2::{Span, TokenStream};
 use quote::{format_ident, quote, ToTokens};
-use syn::{parse::Parse, punctuated::Punctuated, token, Attribute, Error, GenericParam, Generics, Ident, Item, ItemEnum, ItemStruct, Lifetime, LifetimeDef, LitStr, WhereClause, WherePredicate};
+use syn::{token, Attribute, Error, LitStr};
 
 
 fn build_serde_attribute<T: ToTokens>(directives: impl IntoIterator<Item = T>) -> Attribute {
@@ -22,60 +21,11 @@ fn build_serde_attribute<T: ToTokens>(directives: impl IntoIterator<Item = T>) -
 }
 
 pub(super) fn Serialize(input: TokenStream) -> Result<TokenStream, Error> {
-    let mut target = syn::parse2::<Target>(input.clone())?;
-
-    let generics = target.generics().clone();
-    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
-
-    let mut serde_directives = target.remove_serde_directives()?;
-
-    Ok(match serde_directives.iter().position(
-        |d| d.to_string().starts_with("validate")
-    ) {
-        Some(validator_index) => {
-            let validate = serde_directives.remove(validator_index);
-            let validate = syn::parse2::<Validate>(validate)?;
-
-            let mut proxy = target.clone();
-            *proxy.ident_mut() = format_ident!("__serdev_proxy_Serialize_{}__", target.ident());
-            proxy .attrs_mut().push(build_serde_attribute(serde_directives.clone()));
-            target.attrs_mut().push(build_serde_attribute(serde_directives));
-
-            let proxy_ident  = proxy.ident();
-            let target_ident = target.ident();
-
-            let mut where_clause = where_clause.map(ToOwned::to_owned);
-            ↓
-            __HANDLE_BOUNDS__
-
-            quote! {
-                const _: () = {
-                    #[derive(::serdev::__private__::serde::Serialize)]
-                    #[serde(crate = "::serdev::__private__::serde")]
-                    #[allow(non_camel_case_types)]
-                    #proxy
-
-                    impl #impl_generics ::serdev::__private__::serde::Serialize for #target #ty_generics
-                    where #where_clause {
-                        #[inline]
-                        fn serialize<S>(&self, serializer: S) -> ::core::result::Result<S::Ok, S::Error>
-                        where S: ::serdev::__private__::serde::Serializer {
-                            self.0.map_err(::serdev::__private__::serde::ser::Error::custom)?
-                                .serialize(serializer)
-                        }
-                    }
-                };
-            }
-        }
-
-        None => {
-            quote! {
-                #[derive(::serdev::__private__::serde::Serialize)]
-                #[serde(crate = "::serdev::__private__::serde")]
-                #[::serdev::__private__::consume]
-                #target
-            }
-        }
+    Ok(quote! {
+        #[derive(::serdev::__private__::serde::Serialize)]
+        #[serde(crate = "::serdev::__private__::serde")]
+        #[::serdev::__private__::consume]
+        #input
     })
 }
 
@@ -95,7 +45,7 @@ pub(super) fn Deserialize(input: TokenStream) -> Result<TokenStream, Error> {
             let validate = syn::parse2::<Validate>(validate)?;
 
             let mut proxy = target.clone();
-            *proxy.ident_mut() = format_ident!("__serdev_proxy_Serialize_{}__", target.ident());
+            *proxy.ident_mut() = format_ident!("__serdev_proxy_Deserialize_{}__", target.ident());
             proxy .attrs_mut().push(build_serde_attribute(serde_directives.clone()));
             target.attrs_mut().push(build_serde_attribute(serde_directives));
 
