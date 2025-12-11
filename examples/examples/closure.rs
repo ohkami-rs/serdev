32004ac630ecaa68345369a866e5656dd37fc23a
kanarus <kanarus786@gmail.com>
2025-12-11 11:21:20 +0900

    feat: Support any callable expression as `validation`

---
+++ examples/examples/closure.rs
@@ -0,0 +1,24 @@
+use serdev::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(validate = "|p| (p.x * p.y <= 100).then_some(()).ok_or(\"x * y must not exceed 100\")")]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+fn main() {
+    let point = serde_json::from_str::<Point>(r#"
+        { "x" : 1, "y" : 2 }
+    "#).unwrap();
+
+    // Prints point = Point { x: 1, y: 2 }
+    println!("point = {point:?}");
+
+    let error = serde_json::from_str::<Point>(r#"
+        { "x" : 10, "y" : 20 }
+    "#).unwrap_err();
+
+    // Prints error = x * y must not exceed 100
+    println!("error = {error}");
+}
