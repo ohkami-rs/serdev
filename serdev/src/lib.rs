91b843883e085401f547a94cbd206f24e90b69d9
kanarus <kanarus786@gmail.com>
2025-12-13 19:58:02 +0900

    feat: add `derive` feature flag for Cargo.toml-syntax-compatibility with `serde`

--- serdev/src/lib.rs
+++ serdev/src/lib.rs
@@ -1,9 +1,12 @@
 #![cfg_attr(all(doc, not(docsrs)), doc = include_str!("../../README.md"))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
 pub use ::serde::de::{self, Deserialize, Deserializer};
 pub use ::serde::ser::{self, Serialize, Serializer};
+#[cfg(feature = "derive")]
+#[cfg_attr(docsrs, doc(cfg(feature = "derive")))]
 pub use serdev_derive::{Deserialize, Serialize};
-
+#[cfg(feature = "derive")]
 #[doc(hidden)]
 pub mod __private__ {
     pub use ::serde;
